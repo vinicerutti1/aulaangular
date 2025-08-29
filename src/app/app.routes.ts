6b435badc0aa8a79c3f5b2eb15f93db3e468bded
vagnertondelo <vagnertondello@gmail.com>
2025-08-29 19:24:34 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProdutoComponent } from './component/produto-component/produto-component';
+import { HomeComponent } from './component/home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: 'produtos', component: ProdutoComponent},
+{path: '', component: HomeComponent}
+
+];
---
+++ src/app/component/produto-component/produto-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './produto-component.html',
+  styleUrl: './produto-component.css'
+})
+export class ProdutoComponent {
+
+}
